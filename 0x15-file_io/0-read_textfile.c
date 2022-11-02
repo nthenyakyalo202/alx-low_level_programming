eb79db83097b7be964d9347b92121cc168e543d7
nthenyakyalo <nthenyakyalo202@gmail.com>
2022-11-02 16:24:26 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, o_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
