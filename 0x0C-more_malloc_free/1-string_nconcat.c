938c3c8835caf9262c5c6562cde9a296ed717b66
nthenyakyalo <nthenyakyalo202@gmail.com>
2023-02-28 14:38:16 -0800

    solution to task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenates two strings using a
- * most an inputted number of bytes.
- * @s1: first string.
- * @s2: second string.
- * @n: maximum number of bytes of s2 to concatenates to s1.
- * Return: NULL if function fails,
- * pointer to the concatenated space in memory otherwise
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: the number of bytes to include of @s2
+ * Return: newly allocated space in memory;
+ * NULL if the function fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *concat;
-unsigned int len = n, i;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i]; i++)
-len++;
-concat = malloc(sizeof(char) * (len + 1));
-if (concat == NULL)
-return (NULL);
-len = 0;
-for (i = 0; s1[i]; i++)
-concat[len++] = s1[i];
-for (i = 0; s2[i] && i < n;)
-concat[len++] = s2[i];
-concat[len] = '\0';
-return (concat);
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int m;
+	unsigned int p;
+	unsigned int k = 0;
+	unsigned int len;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	if (j > n)
+		j = n;
+
+	len = i + j;
+
+	ar = malloc(sizeof(char) * (len + 1));
+	if (ar == NULL)
+		return (NULL);
+
+	for (p = 0; p < i; p++)
+		ar[k++] = s1[p];
+	for (m = 0; m < j; m++)
+		ar[k++] = s2[m];
+
+	ar[k] = '\0';
+	return (ar);
 }
