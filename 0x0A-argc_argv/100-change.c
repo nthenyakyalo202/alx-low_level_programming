aa7743f438d3c35f712b3b1b6b0960d840375ccf
nthenyakyalo <nthenyakyalo202@gmail.com>
2023-02-28 14:25:46 -0800

    solution to task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,38 @@
-include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
- *  * main - prints the min num of coins to make change for an amount of money
- *   *
- *    * @argc: argument count
- *     * @argv: argument vector
- *      * Return: int
- *       */
+ * main - prints minimum number of coins to make change for an amount of money.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ * Return: 0 if no errors, else 1
+ */
 int main(int argc, char *argv[])
 {
-		unsigned int count = 0;
+	int a, n = 0, i, t;
+	int c[5] = {25, 10, 5, 2, 1};
 
-			if (argc != 2)
-					{
-								printf("Error\n");
-										return (1);
-											}
-				else if (atoi(argv[1]) < 0)
-						{
-									printf("%d\n", 0);
-											return (0);
-												}
-					count = coin_count(count, atoi(argv[1]));
-						printf("%d\n", count);
-							return (0);
-}
-
-/**
- *  * coin_count - counts the min coins needed for change
- *   *
- *    * @count: int to count coins
- *     * @num: argv[1] changed to int
- *      * Return: int
- *       */
-unsigned int coin_count(unsigned int count, int num)
-{
-		unsigned int sum = 0;
-
-			while (!(sum + 25 > (unsigned int)num))
-					{
-								sum += 25;
-										count++;
-											}
-				while (!(sum + 10 > (unsigned int)num))
-						{
-									sum += 10;
-											count++;
-												}
-					while (!(sum + 5 > (unsigned int)num))
-							{
-										sum += 5;
-												count++;
-													}
-						while (!(sum + 2 > (unsigned int)num))
-								{
-											sum += 2;
-													count++;
-														}
-							while (!(sum + 1 > (unsigned int)num))
-									{
-												sum += 1;
-														count++;
-															}
-								return (count);
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
 }
