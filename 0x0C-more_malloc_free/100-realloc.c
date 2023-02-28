fbfc84305d7eadfaebb8aab94b76e805692fcab5
nthenyakyalo <nthenyakyalo202@gmail.com>
2023-02-28 14:33:44 -0800

    solution to task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,42 +1,48 @@
 #include <stdlib.h>
-#include "main.h"
 /**
- * _realloc - reallocates a memory block using malloc and free.
- * @ptr: pointer to the memory previously allocated with a
- * call to ``` malloc : malloc(old_size) ```.
- * @old_size: size in bytes allocated for ptr.
- * @new_size: size in bytes of new memory block.
- *
- * Return: pointer to new mem block, NULL or ptr.
+ * _realloc - reallocates a memory block using malloc and free functions
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: size, in bytes, of the allocated space for @ptr
+ * @new_size: new size, in bytes of the new memory block
+ * Return: pointer to new allocated memory
+ * NULL, if @ptr is NULL (FAILURE) or
+ * NULL, if @new_size == 0 && @ptr != NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	 char *nptr;
-	 unsigned int i;
 
-	 if (new_size == old_size)
-		 return (ptr);
-	 if ((new_size == 0) && (ptr != NULL))
-	 {
-		 free(ptr);
-	   	 return (NULL);
-	 }
-	 if (ptr == NULL)
-	 {
-		 nptr = malloc(new_size);
-		if (nptr == NULL)
-		       	 return (NULL);
-	 }
- 	 if (new_size > old_size && (ptr != NULL))
-	 {
-		nptr = malloc(new_size);
-	        if (nptr == NULL)
-			return (nptr);
-		for (i = 0; i < old_size; i++)
-			nptr[i] = *((char *)ptr + 1);
-	       
-	       	free(ptr);
-	 {
+	unsigned int i;
+	void *new_ptr;
 
-	 	return (nptr);
-}		
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return ('\0');
+
+		return (new_ptr);
+	}
+
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return ('\0');
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (!new_ptr)
+		return ('\0');
+
+	for (i = 0; i < old_size; i++)
+		*((char *)new_ptr + i) = *((char *)ptr + i);
+
+	free(ptr);
+	return ((void *)new_ptr);
+}
+
