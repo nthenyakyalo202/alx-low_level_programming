5e6770014938b348015bb629d9714646f7740aea
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2023-04-15 14:26:18 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,26 +1,41 @@
-#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: first node
- * @str: string to be stored in node
- *
- * Return: address of the new element, NULL if code fails
+ * _strlen - Returns the length of a string.
+ * @s: The string whose the length should be returned.
+ * Return: The length of s.
  */
-
-list_t *add_node(list_t **head, const char *str)
-
+int _strlen(const char *s)
 {
-list_t *temp;
-if (head == NULL)
-return (NULL);
-temp = malloc(sizeof(list_t));
-if (temp == NULL)
-return (NULL);
-temp->str = strdup(str);
-temp->len = strlen(str);
-temp->next = *head;
-*head = temp;
-return (temp);
+	int i = -1;
+	char tmp;
+
+	do {
+		tmp = s[++i];
+	} while (tmp != '\0');
+	return (i);
+}
+
+/**
+ *add_node - Adds a node to a single linked list
+ * @head: A pointer to the pointer to the first element.
+ * @str: The string to initialize with.
+ * Return: The address of the item just added.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *l;
+
+	l = malloc(sizeof(list_t));
+	if (l)
+	{
+		l->str = strdup(str);
+		l->len = _strlen(str);
+		l->next = *head;
+		*head = l;
+	}
+	return (l);
 }
